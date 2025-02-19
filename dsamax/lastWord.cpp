93e881926296dcf4099fb7bac0a5d5d1c3a634c8
Vishal Joshy <vishaljoshi349@gmail.com>
2025-02-19 21:13:24 +0530

    counts number of letters in the last word

---
+++ dsamax/lastWord.cpp
@@ -0,0 +1,26 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> stk;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            } else {
+                if (!stk.empty() && stk.top() == '(' && s[i] == ')') {
+                    stk.pop();
+                } else if (!stk.empty() && stk.top() == '{' && s[i] == '}') {
+                    stk.pop();
+                } else if (!stk.empty() && stk.top() == '[' && s[i] == ']') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
+cd 
