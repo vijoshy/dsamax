bf13e7a21beb7bd52e37137c409352de57c938e9
Vishal Joshy <vishaljoshi349@gmail.com>
2025-02-21 23:02:42 +0530

    Add all digits until it has one digit

---
+++ dsamax/singleDigit.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        int singleDigit(int n) 
+        {
+            while (n>9) 
+            {
+                int sum=0;
+                while(n>0)
+                 {
+                    sum+=n%10;
+                    n/=10;
+                }
+                n=sum;
+            }
+            return n;
+        }
+    };
